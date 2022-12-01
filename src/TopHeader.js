d2679df03d4179b86c08aef71ae95e0977eda720
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-01 21:04:14 +0200

    added styles to top-left corner image

--- src/TopHeader.js
+++ src/TopHeader.js
@@ -4,7 +4,12 @@ import Image from "./pictures/home.png";
 function TopHeader() {
   return (
     <div className="p-2 bg-header bg-cover absolute w-full bg-red-600 h-40">
-      <img className="object-scale-down h-32" src={Image} />
+      <a href="/">
+        <img
+          className="object-scale-down h-32 opacity-50 hover:opacity-100 blur-sm hover:blur-none duration-300"
+          src={Image}
+        />
+      </a>
     </div>
   );
 }
