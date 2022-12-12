1f74262f1c9514050de618d7086f5a85854591a4
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-12 21:17:54 +0200

    burger menu-icon

--- src/LeftNavBar.js
+++ src/LeftNavBar.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import MenuButton from "./MenuButton";
 
 function LeftNavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const changeMenuOpenState = () => setMenuOpen(!menuOpen);
+  const itemsWhenMenuOpen = !menuOpen && "hidden";
+
   return (
     <div className="bg-leftNavBar bg-cover pt-40 lg:col-start-1 lg:col-end-1 bg-black">
-      <p className="text-white">
-        <Link to="/sample1">Sample 1</Link>
-      </p>
-      <p className="text-white">
-        <Link to="/sample2">Sample 2</Link>
-      </p>
-      <p className="text-white">
-        <Link to="/sample3">Sample 3</Link>
-      </p>
-      <p className="text-white">
-        <Link to="/sample4">Sample 4</Link>
-      </p>
+      <MenuButton handleMenuChange={changeMenuOpenState} menuOpen={menuOpen} />
+      <div className={`${itemsWhenMenuOpen} lg:block`}>
+        <p className="text-white">
+          <Link to="/sample1">Sample 1</Link>
+        </p>
+        <p className="text-white">
+          <Link to="/sample2">Sample 2</Link>
+        </p>
+        <p className="text-white">
+          <Link to="/sample3">Sample 3</Link>
+        </p>
+        <p className="text-white">
+          <Link to="/sample4">Sample 4</Link>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/MenuButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MenuButton({ handleMenuChange, menuOpen }) {
+  const bar1WhenMenuOpen = menuOpen && "rotate-45 translate-y-2";
+  const bar2WhenMenuOpen = menuOpen && "opacity-0";
+  const bar3WhenMenuOpen = menuOpen && "-rotate-45 -translate-y-2";
+
+  return (
+    <div className="menu-container lg:hidden" onClick={handleMenuChange}>
+      <div
+        className={`${bar1WhenMenuOpen} duration-100 w-6 h-1 bg-white mt-1`}
+      ></div>
+      <div
+        className={`${bar2WhenMenuOpen} duration-100 w-6 h-1 bg-white mt-1`}
+      ></div>
+      <div
+        className={`${bar3WhenMenuOpen} duration-100 w-6 h-1 bg-white mt-1`}
+      ></div>
+    </div>
+  );
+}
+
+export default MenuButton;
