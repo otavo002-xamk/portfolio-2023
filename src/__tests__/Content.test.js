b8725e6501aadaa42bcd2904002d38af08e8711a
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-13 20:54:00 +0200

    modified Content test

--- src/__tests__/Content.test.js
+++ src/__tests__/Content.test.js
@@ -1,6 +1,23 @@
 import TestRenderer from "react-test-renderer";
 import Content from "../Content";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+const testRouter = createMemoryRouter([
+  {
+    path: "/",
+    element: <Content />,
+    children: [
+      {
+        index: true,
+        element: <p>Child component!</p>,
+      },
+    ],
+  },
+]);
+
+beforeAll(() => render(<RouterProvider router={testRouter} />));
 
 it("renders", () => {
-  TestRenderer.create(<Content />);
+  expect(screen.getByText("Child component!")).toBeInTheDocument();
 });
