787481397fae1dbc62b4d76e19ea7e0a74eb5e07
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-14 10:26:52 +0200

    modified Center.test

--- src/__tests__/Center.test.js
+++ src/__tests__/Center.test.js
@@ -2,10 +2,31 @@ import TestRenderer from "react-test-renderer";
 import Center from "../Center";
 import { BrowserRouter } from "react-router-dom";
 
-it("renders", () => {
-  TestRenderer.create(
-    <BrowserRouter>
-      <Center />
-    </BrowserRouter>
-  );
+const testContent = ["Left Nav-Bar!", "Content!"];
+
+jest.mock("../LeftNavBar", () => () => <p>Left Nav-Bar!</p>);
+jest.mock("../Content", () => () => <p>Content!</p>);
+
+const testRenderer = TestRenderer.create(
+  <BrowserRouter>
+    <Center />
+  </BrowserRouter>
+);
+
+const childComponents = testRenderer.toJSON().children;
+
+it("should render the right amount of child components", () =>
+  expect(childComponents.length).toBe(2));
+
+it.each(childComponents)("should render p $children with no props", (child) => {
+  expect(child.type).toBe("p");
+  expect(child.props.length).toBe(undefined);
 });
+
+it.each([0, 1])(
+  "should render the righ text content for paragraph %i",
+  (index) => {
+    expect(childComponents[index].type).toBe("p");
+    expect(childComponents[index].children[0]).toEqual(testContent[index]);
+  }
+);
