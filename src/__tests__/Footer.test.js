5ea51045234caf15525cf68539d012afea70ee83
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-14 14:03:07 +0200

    modified footer test

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,6 +1,13 @@
 import TestRenderer from "react-test-renderer";
 import Footer from "../Footer";
 
-it("renders", () => {
-  TestRenderer.create(<Footer />);
+const testRenderer = TestRenderer.create(<Footer />);
+const childComponents = testRenderer.toJSON().children;
+
+it("should render the right amount of child components", () =>
+  expect(childComponents.length).toBe(1));
+
+it("should render the righ text content for paragraph", () => {
+  expect(childComponents[0].type).toBe("p");
+  expect(childComponents[0].children[0]).toEqual("Footer");
 });
