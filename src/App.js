616efdc0dcfb27b8efe62768bb4d50a90f49b155
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-19 15:03:07 +0200

    ThemeContext removed, tests fail

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import "./App.css";
 import TopHeader from "./TopHeader";
 import Footer from "./Footer";
 import Center from "./Center";
-import { ThemeContext } from "./theme-context";
 
 function App() {
-  const updateTheme = () => {
-    document.body.classList.contains("dark")
-      ? document.body.classList.remove("dark")
-      : document.body.classList.add("dark");
-  };
-
   return (
-    <ThemeContext.Provider value={{ updateTheme }}>
-      <div className="h-screen bg-red-100 dark:bg-black">
-        <TopHeader />
-        <Center />
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+    <div className="h-screen bg-red-100 dark:bg-black">
+      <TopHeader />
+      <Center />
+      <Footer />
+    </div>
   );
 }
 
--- src/TopHeader.js
+++ src/TopHeader.js
@@ -3,43 +3,44 @@ import { Link } from "react-router-dom";
 import Image from "./pictures/home.png";
 import Sun from "./pictures/sum_03.png";
 import Moon from "./pictures/Crescent-moon.png";
-import { ThemeContext } from "./theme-context";
 
 function TopHeader() {
-  return (
-    <ThemeContext.Consumer>
-      {({ updateTheme }) => (
-        <div className="p-2 bg-header bg-cover absolute w-full bg-red-600 h-40">
-          <div className="float-left">
-            <Link to="/">
-              <img
-                aria-label="home"
-                className="object-scale-down h-32 opacity-50 hover:opacity-100 blur-sm hover:blur-none duration-300"
-                src={Image}
-              />
-            </Link>
-          </div>
+  const updateTheme = () => {
+    document.body.classList.contains("dark")
+      ? document.body.classList.remove("dark")
+      : document.body.classList.add("dark");
+  };
 
-          <div className="float-right">
-            <label
-              htmlFor="toggle-theme"
-              className="flex items-center cursor-pointer relative"
-            >
-              <input
-                type="checkbox"
-                onClick={() => updateTheme()}
-                id="toggle-theme"
-                className="sr-only"
-              />
-              <div className="toggle-bg bg-black h-6 w-12">
-                <img src={Sun} className="h-6 w-6 float-left" />
-                <img src={Moon} className="h-6 w-6 float-right" />
-              </div>
-            </label>
+  return (
+    <div className="p-2 bg-header bg-cover absolute w-full bg-red-600 h-40">
+      <div className="float-left">
+        <Link to="/">
+          <img
+            aria-label="home"
+            className="object-scale-down h-32 opacity-50 hover:opacity-100 blur-sm hover:blur-none duration-300"
+            src={Image}
+          />
+        </Link>
+      </div>
+
+      <div className="float-right">
+        <label
+          htmlFor="toggle-theme"
+          className="flex items-center cursor-pointer relative"
+        >
+          <input
+            type="checkbox"
+            onClick={() => updateTheme()}
+            id="toggle-theme"
+            className="sr-only"
+          />
+          <div className="toggle-bg bg-black h-6 w-12">
+            <img src={Sun} className="h-6 w-6 float-left" />
+            <img src={Moon} className="h-6 w-6 float-right" />
           </div>
-        </div>
-      )}
-    </ThemeContext.Consumer>
+        </label>
+      </div>
+    </div>
   );
 }
 
