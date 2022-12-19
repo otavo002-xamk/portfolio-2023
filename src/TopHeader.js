558c85d8f376302711e0f4b1dff19ee13e7e8630
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-19 11:11:00 +0200

    created theme toggle-switch

--- src/TopHeader.js
+++ src/TopHeader.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Image from "./pictures/home.png";
+import Sun from "./pictures/sum_03.png";
+import Moon from "./pictures/1476842879.png";
 import { ThemeContext } from "./theme-context";
 
 function TopHeader() {
@@ -8,14 +10,33 @@ function TopHeader() {
     <ThemeContext.Consumer>
       {({ theme, updateTheme }) => (
         <div className="p-2 bg-header bg-cover absolute w-full bg-red-600 h-40">
-          <Link to="/">
-            <img
-              aria-label="home"
-              className="object-scale-down h-32 opacity-50 hover:opacity-100 blur-sm hover:blur-none duration-300"
-              src={Image}
-            />
-          </Link>
-          <button onClick={() => updateTheme(theme)}>Hey</button>
+          <div className="float-left">
+            <Link to="/">
+              <img
+                aria-label="home"
+                className="object-scale-down h-32 opacity-50 hover:opacity-100 blur-sm hover:blur-none duration-300"
+                src={Image}
+              />
+            </Link>
+          </div>
+
+          <div className="float-right">
+            <label
+              for="toggle-theme"
+              className="flex items-center cursor-pointer relative"
+            >
+              <input
+                type="checkbox"
+                onClick={() => updateTheme(theme)}
+                id="toggle-theme"
+                className="sr-only"
+              />
+              <div className="toggle-bg bg-black h-6 w-12">
+                <img src={Sun} className="h-6 w-6 float-left" />
+                <img src={Moon} className="h-6 w-6 float-right" />
+              </div>
+            </label>
+          </div>
         </div>
       )}
     </ThemeContext.Consumer>
