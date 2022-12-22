59a79b46164c42fb24e092ea837ed2970f193054
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-22 13:40:58 +0200

    added text-shadows

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-red-900 w-full h-32">
+    <div className="bg-red-900 w-full h-32 text-white text-shadow-menu dark:text-shadow-darkmenu">
       <p>Footer</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const twTextShadow = require("tailwindcss-textshadow");
+
 module.exports = {
   content: ["./src/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
+    textShadow: {
+      default: "1px 1px 1px #7f1d1d",
+      dark: "1px 1px 1px #f87171",
+      menu: "1px 1px 1px #f87171",
+      darkmenu: "1px 1px 1px #7f1d1d",
+    },
     extend: {
       backgroundImage: {
         header: "url('./pictures/jarvi.jpg')",
@@ -13,5 +21,5 @@ module.exports = {
       serif: ["Times New Roman"],
     },
   },
-  plugins: [],
+  plugins: [twTextShadow],
 };
