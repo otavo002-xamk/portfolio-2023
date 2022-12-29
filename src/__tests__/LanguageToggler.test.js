11ecc23eea2dbcc752a07c64c003a963e7e3bb4b
Tapani Voutilainen <ltvoutilainen@outlook.com>
2022-12-29 21:07:35 +0200

    added LanguageToggler test

---
+++ src/__tests__/LanguageToggler.test.js
@@ -0,0 +1,59 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { LanguageContext, languages } from "../language-context";
+import LanguageToggler from "../LanguageToggler";
+
+jest.mock("../language-context");
+const mockFn = jest.fn();
+const updateLanguage = (event) => mockFn(event.target.alt);
+
+jest.mock("react-select", () => (props) => {
+  mockFn(props);
+
+  return <div onClick={props.onChange}>{props.placeholder}</div>;
+});
+
+describe("With english language", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const language = languages.en;
+    render(
+      <LanguageContext.Provider value={{ language, updateLanguage }}>
+        <LanguageToggler />
+      </LanguageContext.Provider>
+    );
+  });
+
+  it("should render", () => {
+    expect(screen.queryByAltText("fi")).not.toBeInTheDocument();
+    expect(screen.getByAltText("en")).toBeInTheDocument();
+  });
+
+  it("should call updateLanguage() when clicked", () => {
+    fireEvent.click(screen.getByAltText("en"));
+    expect(mockFn).not.toHaveBeenCalledWith("fi");
+    expect(mockFn).toHaveBeenCalledWith("en");
+  });
+});
+
+describe("With finnish language", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const language = languages.fi;
+    render(
+      <LanguageContext.Provider value={{ language, updateLanguage }}>
+        <LanguageToggler />
+      </LanguageContext.Provider>
+    );
+  });
+
+  it("should render", () => {
+    expect(screen.getByAltText("fi")).toBeInTheDocument();
+    expect(screen.queryByAltText("en")).not.toBeInTheDocument();
+  });
+
+  it("should call updateLanguage() when clicked", () => {
+    fireEvent.click(screen.getByAltText("fi"));
+    expect(mockFn).toHaveBeenCalledWith("fi");
+    expect(mockFn).not.toHaveBeenCalledWith("en");
+  });
+});
