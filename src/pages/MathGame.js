6c6c2873ebe9e889c5b16bdccc0577505c65815a
Tapani Voutilainen <ltvoutilainen@outlook.com>
2023-01-25 21:29:10 +0200

    added MathGame table buttons & modified layout

--- src/pages/MathGame.js
+++ src/pages/MathGame.js
@@ -19,6 +19,12 @@ function MathGame() {
 
   const tableOfOptions = randomResults.sort();
 
+  const chooseAnswer = (event) => {
+    event.target.id.slice(4) == sum
+      ? console.log("correct!")
+      : console.log("false!");
+  };
+
   return (
     <LanguageContext.Consumer>
       {({ language }) => (
@@ -27,44 +33,66 @@ function MathGame() {
             {language.pages.mathGame.content}
           </h1>
           <br />
-          <div className="flex items-stretch">
-            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
+          <div className="grid grid-rows-2 tablet:flex tablet:items-stretch">
+            <div className="row-start-1 row-span-1 self-center text-center py-2 bg-rose-200 tablet:w-2/12">
               {randomNumbers[0]}
             </div>
-            <div className="self-center text-center py-2 w-1/12">+</div>
-            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
+            <div className="row-start-1 row-span-1 self-center text-center py-2 tablet:w-1/12">
+              +
+            </div>
+            <div className="row-start-1 row-span-1 self-center text-center py-2 bg-rose-200 tablet:w-2/12">
               {randomNumbers[1]}
             </div>
-            <div className="self-center text-center py-2 w-1/12">+</div>
-            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
+            <div className="row-start-1 row-span-1 self-center text-center py-2 tablet:w-1/12">
+              +
+            </div>
+            <div className="row-start-1 row-span-1 self-center text-center py-2 bg-rose-200 tablet:w-2/12">
               {randomNumbers[2]}
             </div>
-            <div className="self-center text-center py-2 w-1/12">=</div>
-            <table className="w-2/12 border-separate">
+            <div className="row-start-1 row-span-1 self-center text-center py-2 tablet:w-1/12">
+              =
+            </div>
+            <table className="col-start-2 col-span-2 tablet:w-2/12 border-separate">
               <tbody>
                 <tr>
-                  <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
+                  <td
+                    id={`td1-${tableOfOptions[0]}`}
+                    onClick={chooseAnswer}
+                    className="cursor-pointer shadow-mathBox w-full text-center py-2 bg-red-400"
+                  >
                     {tableOfOptions[0]}
                   </td>
                 </tr>
                 <tr>
-                  <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
+                  <td
+                    id={`td2-${tableOfOptions[1]}`}
+                    onClick={chooseAnswer}
+                    className="cursor-pointer shadow-mathBox w-full text-center py-2 bg-red-400"
+                  >
                     {tableOfOptions[1]}
                   </td>
                 </tr>
                 <tr>
-                  <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
+                  <td
+                    id={`td3-${tableOfOptions[2]}`}
+                    onClick={chooseAnswer}
+                    className="cursor-pointer shadow-mathBox w-full text-center py-2 bg-red-400"
+                  >
                     {tableOfOptions[2]}
                   </td>
                 </tr>
                 <tr>
-                  <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
+                  <td
+                    id={`td4-${tableOfOptions[3]}`}
+                    onClick={chooseAnswer}
+                    className="cursor-pointer shadow-mathBox w-full text-center py-2 bg-red-400"
+                  >
                     {tableOfOptions[3]}
                   </td>
                 </tr>
               </tbody>
             </table>
-            <div className="self-center float-left text-center py-2 w-1/12">
+            <div className="self-center float-left text-center py-2 tablet:w-1/12">
               ?
             </div>
           </div>
