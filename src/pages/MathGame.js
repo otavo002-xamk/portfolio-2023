29374d446c3239bab4dc6866ea061ec6bfd071a1
Tapani Voutilainen <ltvoutilainen@outlook.com>
2023-01-25 19:57:12 +0200

    modified MathGame early layout & results

--- src/pages/MathGame.js
+++ src/pages/MathGame.js
@@ -3,10 +3,21 @@ import { LanguageContext } from "../language-context";
 
 function MathGame() {
   const randomNumbers = [
-    (Math.random() * 100).toFixed(0),
-    (Math.random() * 100).toFixed(0),
-    (Math.random() * 100).toFixed(0),
+    Number((Math.random() * 100).toFixed(0)),
+    Number((Math.random() * 100).toFixed(0)),
+    Number((Math.random() * 100).toFixed(0)),
   ];
+
+  const sum = randomNumbers[0] + randomNumbers[1] + randomNumbers[2];
+
+  const randomResults = [
+    (Math.random() * 300).toFixed(0),
+    (Math.random() * 300).toFixed(0),
+    (Math.random() * 300).toFixed(0),
+    sum,
+  ];
+
+  const tableOfOptions = randomResults.sort();
 
   return (
     <LanguageContext.Consumer>
@@ -16,44 +27,46 @@ function MathGame() {
             {language.pages.mathGame.content}
           </h1>
           <br />
-          <div>
-            <div className="shadow-mathBox float-left text-center py-2 bg-rose-400 w-2/12">
+          <div className="flex items-stretch">
+            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
               {randomNumbers[0]}
             </div>
-            <div className="float-left text-center py-2 w-1/12">+</div>
-            <div className="shadow-mathBox float-left text-center py-2 bg-rose-400 w-2/12">
+            <div className="self-center text-center py-2 w-1/12">+</div>
+            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
               {randomNumbers[1]}
             </div>
-            <div className="float-left text-center py-2 w-1/12">+</div>
-            <div className="shadow-mathBox float-left text-center py-2 bg-rose-400 w-2/12">
+            <div className="self-center text-center py-2 w-1/12">+</div>
+            <div className="self-center text-center py-2 bg-rose-200 w-2/12">
               {randomNumbers[2]}
             </div>
-            <div className="float-left text-center py-2 w-1/12">=</div>
-            <table className="float-left w-2/12 border-separate">
+            <div className="self-center text-center py-2 w-1/12">=</div>
+            <table className="w-2/12 border-separate">
               <tbody>
                 <tr>
                   <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
-                    300
+                    {tableOfOptions[0]}
                   </td>
                 </tr>
                 <tr>
                   <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
-                    300
+                    {tableOfOptions[1]}
                   </td>
                 </tr>
                 <tr>
                   <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
-                    300
+                    {tableOfOptions[2]}
                   </td>
                 </tr>
                 <tr>
                   <td className="shadow-mathBox w-full text-center py-2 bg-red-400">
-                    300
+                    {tableOfOptions[3]}
                   </td>
                 </tr>
               </tbody>
             </table>
-            <div className="float-left text-center py-2 w-1/12">?</div>
+            <div className="self-center float-left text-center py-2 w-1/12">
+              ?
+            </div>
           </div>
         </div>
       )}
