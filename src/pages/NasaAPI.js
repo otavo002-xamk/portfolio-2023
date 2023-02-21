6ca0b7468d0c5e5472e644f656e18af331c35c0e
Tapani Voutilainen <ltvoutilainen@outlook.com>
2023-02-21 21:15:55 +0200

    NasaAPI under development

--- src/pages/NasaAPI.js
+++ src/pages/NasaAPI.js
@@ -3,25 +3,38 @@ import { LanguageContext } from "../language-context";
 
 function NasaAPI() {
   const [nasaPictures, setNasaPictures] = useState([]);
+  const [sol, setSol] = useState("");
+  const [tooBigNumber, setTooBigNumber] = useState(false);
 
-  const getImages = () => {
-    fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=56&api_key=oe6EW1xJH9VdPM26LRV6fwrAMEedCQxcI53robyv`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        let i = 0;
-        let images = [];
-        data.photos.forEach((element) => {
-          while (i < 5) {
-            images.push(element);
-            i++;
-          }
-          setNasaPictures(images);
+  const getImages = (sol) => {
+    if (sol < 3496) {
+      fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=oe6EW1xJH9VdPM26LRV6fwrAMEedCQxcI53robyv`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let i = 0;
+          let images = [];
+
+          data.photos.forEach((element) => {
+            while (i < 5) {
+              images.push(element);
+              i++;
+            }
+
+            setNasaPictures(images);
+          });
         });
-      });
+      tooBigNumber && setTooBigNumber(false);
+    } else {
+      setTooBigNumber(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSol(e.target.value);
   };
 
   return (
@@ -29,7 +42,20 @@ function NasaAPI() {
       {({ language }) => (
         <div className="p-12 lg:p-0">
           <h1 className="text-2xl">{language.pages.nasaAPI.title}</h1>
-          <button onClick={getImages}>Get images from NASA.</button>
+          <label htmlFor="nasa-input">Insert sol please:</label>
+
+          <input
+            value={sol}
+            onChange={handleChange}
+            id="nasa-input"
+            type="text"
+            placeholder="122"
+          />
+
+          <button onClick={() => getImages(sol)}>Get images from NASA.</button>
+          <br />
+          {tooBigNumber && <p>Too big number!</p>}
+
           {nasaPictures.map((picture, i) => (
             <img src={picture.img_src} alt={`nasa-${i}`} />
           ))}
