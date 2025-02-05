8e481a3967aeb07a988d0c350b38a45588ba9544
Tapani Voutilainen <ltvoutilainen@outlook.com>
2025-02-05 18:27:58 +0200

    added links

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { LanguageContext } from "../language-context";
+import React from 'react'
+import { LanguageContext } from '../language-context'
 
 function Links() {
   return (
@@ -9,27 +9,42 @@ function Links() {
           <h1 className="text-2xl">{language.pages.links.title}</h1>
           <br />
           <br />
-          <div>
+          <div className="clear-both">
             <div className="md:w-1/4 md:float-left">
               <p className="mb-8">
                 <b>Git Repositories:</b>
               </p>
             </div>
-            <div className="md:w-3/4 md:float-right pl-12">
+            <div className="md:w-3/4 md:float-right pl-12 mb-8">
+              <a href="https://gitea.com/woudiiii-girps">
+                https://gitea.com/woudiiii-girps
+              </a>
+              <br />
               <a href="https://sourceforge.net/u/woudiiii-girps/profile">
                 https://sourceforge.net/u/woudiiii-girps/profile
               </a>
-              <br />
               <br />
               <a href="https://github.com/otavo002-xamk">
                 https://github.com/otavo002-xamk
               </a>
             </div>
           </div>
+          <div className="clear-both">
+            <div className="md:w-1/4 md:float-left">
+              <p className="mb-8">
+                <b>Docker Hub:</b>
+              </p>
+            </div>
+            <div className="md:w-3/4 md:float-right pl-12 mb-8">
+              <a href="https://hub.docker.com/u/woudisnakes">
+                https://hub.docker.com/u/woudisnakes
+              </a>
+            </div>
+          </div>
         </div>
       )}
     </LanguageContext.Consumer>
-  );
+  )
 }
 
-export default Links;
+export default Links
