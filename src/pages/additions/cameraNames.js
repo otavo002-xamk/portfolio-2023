6d2f740213a3bd6d43a4073db5d83f2f435a589d
Tapani Voutilainen <ltvoutilainen@outlook.com>
2025-12-24 17:19:13 +0200

    removed  2 camera names

--- src/pages/additions/cameraNames.js
+++ src/pages/additions/cameraNames.js
@@ -1,14 +1,9 @@
 export const cameraNames = [
-  { abbreviation: "FHAZ", Name: "Front Hazard Avoidance Camera" },
-  { abbreviation: "RHAZ", Name: "Rear Hazard Avoidance Camera" },
-  { abbreviation: "MAST", Name: "Mast Camera" },
-  { abbreviation: "CHEMCAM", Name: "Chemistry and Camera Complex" },
-  { abbreviation: "MAHLI", Name: "Mars Hand Lens Imager" },
-  { abbreviation: "MARDI", Name: "Mars Descent Imager" },
-  { abbreviation: "NAVCAM", Name: "Navigation Camera" },
-  { abbreviation: "PANCAM", Name: "Panoramic Camera" },
-  {
-    abbreviation: "MINTES",
-    Name: "Miniature Thermal Emission Spectrometer (Mini-TES)",
-  },
-];
+  { abbreviation: 'FHAZ', Name: 'Front Hazard Avoidance Camera' },
+  { abbreviation: 'RHAZ', Name: 'Rear Hazard Avoidance Camera' },
+  { abbreviation: 'MAST', Name: 'Mast Camera' },
+  { abbreviation: 'CHEMCAM', Name: 'Chemistry and Camera Complex' },
+  { abbreviation: 'MAHLI', Name: 'Mars Hand Lens Imager' },
+  { abbreviation: 'MARDI', Name: 'Mars Descent Imager' },
+  { abbreviation: 'NAVCAM', Name: 'Navigation Camera' },
+]
